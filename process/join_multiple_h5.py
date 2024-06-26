4f6cc9f43357189e50c4733c30f8eaa2d61fb161
Antonio Mirarchi <antonio.mirarchi@upf.edu>
2024-06-26 11:58:46 +0200

    to black

--- process/join_multiple_h5.py
+++ process/join_multiple_h5.py
@@ -1,30 +1,34 @@
+import h5py
+import shutil
+import tempfile
+from glob import glob
 from tqdm import tqdm
 from os.path import join as opj
-from glob import glob
-import h5py 
-import tempfile
-import shutil
-import json
+
+
 def sorter(filelist):
-    """ returns a sorted list of files based on the last number (batch number) in the filename"""
+    """returns a sorted list of files based on the last number (batch number) in the filename"""
     sortdict = {}
     for file in filelist:
-        sortdict[int(file.split('_')[-1].split('.')[0])] = file
+        sortdict[int(file.split("_")[-1].split(".")[0])] = file
     return [sortdict[key] for key in sorted(sortdict.keys())]
 
-if __name__ == '__main__':
-    batches_dir = 'batch_files/'
-    base_filename = 'mdcath_analysis' # or 'mdcath_source
+
+if __name__ == "__main__":
+    # Set the directory and base filename (mdcath_analysis or mdcath_source)
+    batches_dir = "h5files/batch_files/"
+    base_filename = "mdcath_analysis"
+
     h5_list = sorter(glob(opj(batches_dir, f"{base_filename}_*.h5")))
     with tempfile.TemporaryDirectory() as temp:
-        with h5py.File(opj(temp, 'merged.h5'), 'w') as merged:
+        with h5py.File(opj(temp, "merged.h5"), "w") as merged:
             for h5_file in tqdm(h5_list, total=len(h5_list), desc="Merging"):
-                with h5py.File(h5_file, 'r') as h5:
+                with h5py.File(h5_file, "r") as h5:
                     for key in h5.keys():
                         if key in merged.keys():
                             print(f"Key {key} already in merged file")
                             continue
                         merged.copy(h5[key], key)
-                        
-        shutil.copyfile(opj(temp, 'merged.h5'), f'./{base_filename}.h5')
+
+        shutil.copyfile(opj(temp, "merged.h5"), f"./{base_filename}.h5")
         print(f"Merged file saved to {base_filename}.h5')")
